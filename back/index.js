95318d8c309d6f90aa664fcf92e774ba4f5ce3b8
Marie Beaujeu <marie.beaujeu@gmail.com>
2021-11-02 14:45:26 +0100

    Initial commit

---
+++ back/index.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const express = require('express');
+const rateLimit = require('express-rate-limit');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+const recipe = require('./recipe/index');
+
+app.use(express.json());
+
+const whitelist = ['http://127.0.0.1', 'http://127.0.0.1:5500', 'http://localhost:3000'];
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (!origin || whitelist.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+
+const limiter = rateLimit({
+    windowMs: 1000,
+    max: 1
+});
+app.use(limiter);
+app.use('/recipe', recipe);
+
+app.listen(port, () => console.log(`App listening on port : ${port}`))
---
+++ back/recipe/index.js
@@ -0,0 +1,46 @@
+require('dotenv').config();
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+
+const fetchRecipes = async(searchText) => {
+    const url = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.API_RECIPE_KEY}&query=${searchText}&number=10`;
+    try {
+        const result = await axios({
+            method: 'get',
+            url: url
+        });
+        const response = await result.data;
+        return response;
+    } catch (e) {
+        return {error: e.stack};
+    }
+}
+
+const fetchRecipeById = async(id) => {
+    const url = `https://api.spoonacular.com/recipes/${id}/information?apiKey=${process.env.API_RECIPE_KEY}`;
+    try {
+        const result = await axios({
+            method: 'get',
+            url: url
+        });
+        const response = await result.data;
+        return response;
+    } catch(e) {
+        return {error: e.stack};
+    }
+}
+
+router.get('/search/:searchText', async(req, res) => {
+    const searchText = req.params.searchText;
+    const data = await fetchRecipes(searchText);
+    res.json(data);
+});
+
+router.get('/:id', async (req, res) => {
+    const id = parseInt(req.params.id, 10);
+    const data = await fetchRecipeById(id);
+    res.json(data);
+})
+
+module.exports = router;
