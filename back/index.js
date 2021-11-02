94a147297d358a3ab82ef261d27ecd967740de30
Marie Beaujeu <marie.beaujeu@gmail.com>
2021-11-02 16:59:45 +0100

    Update cors access origin bis

--- back/index.js
+++ back/index.js
@@ -9,17 +9,20 @@ const recipe = require('./recipe/index');
 app.use(express.json());
 
 const whitelist = ['http://127.0.0.1', 'http://127.0.0.1:5500', 'http://localhost:3000', 'https://rima-chan.github.io', 'https://git.heroku.com/recipe-js.git'];
+// const corsOptions = {
+//     origin: (origin, callback) => {
+//         if (!origin || whitelist.indexOf(origin) !== -1) {
+//             callback(null, true);
+//         } else {
+//             callback(new Error('Not allowed by CORS'));
+//         }
+//     },
+//     methods: ['GET'],
+//     optionsSuccessStatus: 200
+// };
 const corsOptions = {
-    origin: (origin, callback) => {
-        if (!origin || whitelist.indexOf(origin) !== -1) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    methods: ['GET'],
-    optionsSuccessStatus: 200
-};
+    origin: whitelist
+}
 app.use(cors(corsOptions));
 
 const limiter = rateLimit({
